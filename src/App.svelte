c32c2483ca823ed7882dc451bb610a9cc63eea7a
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-11 08:58:10 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  function handleClick(){
+    alert('you have clicked!')
+  }
+</script>
+
+<button on:click={handleClick}>
+  Click me! on:click
+</button>
+
+<!-- two modifiers are chained together "click|once" -->
+<button on:click|once={handleClick}>
+  Click me! on:click|once
+</button>
+
+<!-- <button on:click={handleClick}> -->
+<!-- three modifiers are chained together "click|once|capture" -->
+<button on:click|once|capture={handleClick}>
+  Click me! on:click|once|capture
+</button>
